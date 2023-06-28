b11466cc5b33172d5f00ee413b227e49648ca3a9
lnmcc <lnmcc@hotmail.com>
2023-06-28 18:32:15 +0800

    add

--- test10.py
+++ test10.py
@@ -79,7 +79,11 @@ model = tf.keras.Sequential(
     ]
 )
 
-model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+adam = tf.keras.optimizers.Adam(
+    learning_rate=0.0001, beta_1=0.9, beta_2=0.999, amsgrad=False
+)
+
+model.compile(loss="binary_crossentropy", optimizer=adam, metrics=["accuracy"])
 model.summary()
 
 model.fit(training_padded, training_labels, epochs=50)
