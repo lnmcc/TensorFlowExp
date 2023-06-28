dcf9f4f7791035f2b4d9f274ae1a1dfd0a107b99
lnmcc <lnmcc@hotmail.com>
2023-06-28 17:26:48 +0800

    add

--- test10.py
+++ test10.py
@@ -1,7 +1,86 @@
 import tensorflow as tf
+import json
+import string
+import numpy as np
+from bs4 import BeautifulSoup
 from tensorflow import keras
 import tensorflow_datasets as tfds
 
 # import tensorflow_addons as tfa
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+stopwords = ["a", ..., "yourselves"]
+table = str.maketrans("", "", string.punctuation)
+
+with open("sarcasm.json", "r") as f:
+    datastore = json.load(f)
+
+sentences = []
+labels = []
+urls = []
+
+for item in datastore:
+    sentence = item["headline"].lower()
+    sentence = sentence.replace(",", " , ")
+    sentence = sentence.replace(".", " . ")
+    sentence = sentence.replace("-", " - ")
+    sentence = sentence.replace("/", " / ")
+    soup = BeautifulSoup(sentence)
+    sentence = soup.get_text()
+    words = sentence.split()
+    filtered_sentence = ""
+    for word in words:
+        word = word.translate(table)
+        if word not in stopwords:
+            filtered_sentence = filtered_sentence + word + " "
+    sentences.append(filtered_sentence)
+    labels.append(item["is_sarcastic"])
+    urls.append(item["article_link"])
+
+training_size = 23000
+training_sentences = sentences[0:training_size]
+testing_sentences = sentences[training_size:]
+training_labels = labels[0:training_size]
+testing_labels = labels[training_size:]
+
+vocab_size = 20000
+max_length = 10
+trunc_type = "post"
+padding_type = "post"
+oov_tok = "<OOV>"
+
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(training_sentences)
+
+word_index = tokenizer.word_index
+
+training_sentences = tokenizer.texts_to_sequences(training_sentences)
+training_padded = pad_sequences(training_sentences, padding=padding_type)
+
+testing_sentences = tokenizer.texts_to_sequences(testing_sentences)
+testing_padded = pad_sequences(testing_sentences, padding=padding_type)
+
+training_padded = np.array(training_padded)
+training_labels = np.array(training_labels)
+testing_padded = np.array(testing_padded)
+testing_labels = np.array(testing_labels)
+
+print(training_padded)
+print(training_labels)
+
+model = tf.keras.Sequential(
+    [
+        tf.keras.layers.Embedding(20000, 16),
+        tf.keras.layers.GlobalAveragePooling1D(),
+        tf.keras.layers.Dense(24, activation="relu"),
+        tf.keras.layers.Dense(1, activation="sigmoid"),
+    ]
+)
+
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+model.summary()
+
+model.fit(training_padded, training_labels, epochs=50)
+model.evaluate(testing_padded, testing_labels)
