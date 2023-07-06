d8c80982f3ea80e4ba4bb36f4d97b264a58127d3
lnmcc <lnmcc@hotmail.com>
2023-07-06 14:42:12 +0800

    Update test12.py

--- test12.py
+++ test12.py
@@ -14,8 +14,24 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 data = "In the town of Athy one Jeremy Lanigan \n Battered away til he hadnt a pound. \n His father died and made him a man again \n Left him a farm and ten acres of ground. \n He gave a grand party for friends and relations \n Who didnt forget him when come to the wall, \n And if youll but listen Ill make your eyes glisten \n Of the rows and the ructions of Lanigan’s Ball. \n Myself to be sure got free invitation, \n For all the nice girls and boys I might ask, \n And just in a minute both friends and relations \n Were dancing round merry as bees round a cask. \n Judy ODaly, that nice little milliner, \n She tipped me a wink for to give her a call, \n And I soon arrived with Peggy McGilligan \n Just in time for Lanigans Ball."
 corpus = data.lower().split("\n")
-print(corpus)
+# print(corpus)
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(corpus)
 total_words = len(tokenizer.word_index) + 1
-print(tokenizer.word_index)
+# print(tokenizer.word_index)
+
+input_sequences = []
+for line in corpus:
+    token_list = tokenizer.texts_to_sequences([line])[0]
+    for i in range(1, len(token_list)):
+        n_gram_sequence = token_list[: i + 1]
+        input_sequences.append(n_gram_sequence)
+max_sequence_len = max([len(x) for x in input_sequences])
+input_sequences = np.array(
+    pad_sequences(input_sequences, maxlen=max_sequence_len, padding="pre")
+)
+print(input_sequences[:5])
+xs, labels = input_sequences[:, :-1], input_sequences[:, -1]
+print(labels)
+ys = tf.keras.utils.to_categorical(labels, num_classes=total_words)
+print(ys)
