2ef861f96b11fabf64189eb88289a319268c3649
lnmcc <lnmcc@hotmail.com>
2023-07-07 16:49:32 +0800

    add

--- test13.py
+++ test13.py
@@ -12,7 +12,7 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 
-data = "In the town of Athy one Jeremy Lanigan \n Battered away til he hadnt a pound. \n His father died and made him a man again \n Left him a farm and ten acres of ground. \n He gave a grand party for friends and relations \n Who didnt forget him when come to the wall, \n And if youll but listen Ill make your eyes glisten \n Of the rows and the ructions of Lanigan’s Ball. \n Myself to be sure got free invitation, \n For all the nice girls and boys I might ask, \n And just in a minute both friends and relations \n Were dancing round merry as bees round a cask. \n Judy ODaly, that nice little milliner, \n She tipped me a wink for to give her a call, \n And I soon arrived with Peggy McGilligan \n Just in time for Lanigans Ball."
+data = open("irish-lyrics-eof.txt").read()
 corpus = data.lower().split("\n")
 # print(corpus)
 tokenizer = Tokenizer()
@@ -37,23 +37,13 @@ ys = tf.keras.utils.to_categorical(labels, num_classes=total_words)
 
 model = tf.keras.Sequential()
 model.add(tf.keras.layers.Embedding(total_words, 8))
-model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(max_sequence_len - 1)))
+model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(max_sequence_len - 1,return_sequences='True')))
+model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(max_sequence_len-1)))
 model.add(tf.keras.layers.Dense(total_words, activation="Softmax"))
 
-epochs = 1500
+epochs = 1000
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 history = model.fit(xs, ys, epochs=epochs, verbose=1)
-
-seed_text = "in the town of athy"
-token_list = tokenizer.texts_to_sequences([seed_text])[0]
-token_list = pad_sequences([token_list], maxlen=max_sequence_len - 1, padding="pre")
-predicated = np.argmax(model.predict(token_list), axis=-1)
-print(predicated)
-
-for word, index in tokenizer.word_index.items():
-    if index == predicated:
-        print(word)
-        break
 
 seed_tex = "sweet jeremy saw dublin"
 next_words = 10
